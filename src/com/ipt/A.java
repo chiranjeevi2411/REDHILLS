a5ed6822fe7d359c7ca66d15688c04e6d703ec55
chiranjeevi2411 <pgchiranjeevi@gmail.com>
2023-09-01 20:36:02 +0530

    welcome

--- src/com/ipt/A.java
+++ src/com/ipt/A.java
@@ -3,6 +3,11 @@ package com.ipt;
 public class A {
 	private void fgfgf() {
 		
+		System.out.println("welcome to redhills");
 	}
 
+	public static void main(String[] args) {
+		A b = new A();
+		b.fgfgf();
+	}
 }
